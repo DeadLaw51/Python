034df9a78ccf54592a50bb227a2935705398b801
Anonym <anonym516836@gmail.com>
2023-09-26 17:20:15 +0300

    dz_22

---
+++ Semenar_4/DZ/Z_22.py
@@ -0,0 +1,11 @@
+import random
+kol_1 = int(input("Введите размер 1 множества - "))
+kol_2 = int(input("Введите размер 2 множества - "))
+
+my_set_1 = {random.randint(0,20) for i in range(kol_1) }
+my_set_2 = {random.randint(0,20) for i in range(kol_2) }
+
+print(my_set_1)
+print(my_set_2)
+
+print(my_set_1 & my_set_2)
